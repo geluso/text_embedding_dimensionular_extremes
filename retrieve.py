62d5a022ba53496c70813e1313d6d59cf687b7b5
Steve Geluso <stevegeluso@gmail.com>
2025-02-02 18:51:00 -0800

    showing top and bottom three words

--- retrieve.py
+++ retrieve.py
@@ -50,20 +50,41 @@ print()
 
 print('total docs:', len(docs['documents']))
 
+class MaxThree:
+  def __init__(self):
+      self.values = []
+
+  def add(self, value, item):
+    self.values.append([value, item])
+    self.values.sort(key=lambda x: x[0])
+    if len(self.values) > 3:
+      self.values.pop(0)
+  
+  def __str__(self):
+    return f"{self.values[0][1]},{self.values[1][1]},{self.values[2][1]} {self.values[2][0]}"
+
+class MinThree:
+  def __init__(self):
+      self.values = []
+
+  def add(self, value, item):
+    self.values.append([value, item])
+    self.values.sort(key=lambda x: x[0])
+    if len(self.values) > 3:
+      self.values.pop()
+  
+  def __str__(self):
+    return f"{self.values[0][0]} {self.values[0][1]},{self.values[1][1]},{self.values[2][1]}"
+
 for i in range(4096):
-  min_val = 0
-  max_val = 0
-  min_word = None
-  max_word = None
+  min_three = MinThree()
+  max_three = MaxThree()
   for j in range(len(docs['documents'])):
     word = docs['documents'][j]  # This is the raw numpy array
     vector = docs['embeddings'][j]  # This is the raw numpy array
-    if vector[i] < min_val:
-      min_val = vector[i]
-      min_word = word
-    if vector[i] > max_val:
-      max_val = vector[i]
-      max_word = word
-  print(i, min_val, min_word, max_word, max_val)
+    vector_val = vector[i]
+    min_three.add(vector_val, word)
+    max_three.add(vector_val, word)
+  print(i, str(min_three), str(max_three))
 
 print(docs['embeddings'][0])
